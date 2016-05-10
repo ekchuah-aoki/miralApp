2fe2a754aeb55ae7260bd2e0d7bfbd5c8781c6b5
yumenoarcher <yumenoarcher@192.168.1.73>
2016-05-10 16:27:25 +0900

    2016/5/10

--- www/beauti/home/announce_detail/announce_detail_ctl.js
+++ www/beauti/home/announce_detail/announce_detail_ctl.js
@@ -2,22 +2,6 @@ angular.module('miral.beauti.home.announce.detail.controllers', [])
 
 .controller('beautiHomeAnnounceDetailControllers', function($scope, $state,$ionicPopup) {
 
-	$scope.favs = ['あさの','よしだ','パンダ','りんご'];
-	
-   // A confirm dialog
-   $scope.showConfirm = function() {
-     var confirmPopup = $ionicPopup.confirm({
-       title: '予約キャンセル',
-       cssClass: 'cancel_button',
-       template: '<dl><dt>この予約をキャンセルしてしまってよろしいですか？</dt><dd>店舗名：salon Hair Desein</dd><dd>予約日時：2016/00/00 12:00~14:00</dd></dl>',
-       cancelText:'閉じる',
-       okType:'c01',
-       okText: 'キャンセルする'
-     });
-   };
-
-
-
 
 
 })
