3a4847f9da7a18e75fd73c1ef7843e6bfb41c8a2
y.aoki <y.aoki@192.168.1.52>
2016-05-16 13:39:55 +0900

    修正

--- www/common/imageUtil.js
+++ www/common/imageUtil.js
@@ -5,11 +5,14 @@ angular.module('miral.common.imageUtil', ['miral.loginInfo'])
 	var myself = {
 			///////////////////////////////
 			//ライセンスイメージ選択
-			showImagPick:function(sucess_, fail_,width_, height_){
-				imagePicker.getPictures(sucess_, fail_, 	
+			
+			showImagPick:function(sucess_, fail_,width_, height_, count_){
+
+				
+				imagePicker.getPictures(sucess_, fail_, opt_	
 						  { // options object, all optional
 							title:"アルバム",  
-						    maximumImagesCount: 1, // Android only since plugin version 2.1.1, default no limit
+						    maximumImagesCount: count_, // Android only since plugin version 2.1.1, default no limit
 						    quality: 70, // 0-100, default 100 which is highest quality
 						    width_, //width: 100,  // proportionally rescale image to this width, default no rescale
 						    height_, //height: 400, // same for height
