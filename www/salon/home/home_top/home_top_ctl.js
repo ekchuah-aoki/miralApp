7e60be9b7530d3acb6a235edcf7fb93ff85f470f
m.nagashima <m.nagashima@192.168.1.21>
2016-05-17 14:52:23 +0900

    20160517 salon

--- www/salon/home/home_top/home_top_ctl.js
+++ www/salon/home/home_top/home_top_ctl.js
@@ -2,9 +2,43 @@ angular.module('miral.salon.home.home_top.controllers', [])
 
 .controller('salonHomeHomeTopControllers', function($scope, $state) {
 
-
-
+	// = 遷移 = = = = = = = = = =
+	//サロン：ホーム：シェアリクエスト
+	$scope.salonScheduleShareRequest=function() {
+		$state.go('salon-schedule-share_request',null,'');
+	}
 	
+	//サロン：スケジュール：メッセージ
+	$scope.salonScheduleMessageList=function() {
+		$state.go('salon-schedule-message_list',null,'');
+	}
+	
+	//サロン：スケジュール：スケジュール
+	$scope.salonScheduleReservToday=function() {
+		$state.go('salon-schedule-reserv_today',null,'');
+	}
+	
+	//サロン：ホーム：おしらせ詳細
+	$scope.salonHomeAnnounceDetail=function() {
+		$state.go('salon-home-anounce_detail',null,'');
+	}
+
+	//サロン：ホーム：おしらせ一覧
+	$scope.salonHomeAnnounce=function() {
+		$state.go('salon-home-anounce',null,'');
+	}
+
+	//美容師：ホーム：ミラルのてびき
+	$scope.salonHomeGuidesMiral=function() {
+		$state.go('beauti-home-guides_miral',null,'');
+	}
+	
+	//サロン：ホーム：ご意見お問い合わせ
+	$scope.salonHomeRequest=function() {
+		$state.go('salon-home-request',null,'');
+	}
+
+
 })
 
 ;
