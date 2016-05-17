2ee6d35331d75a2c5bc9481a7959e5c26f4ec18b
m.nagashima <m.nagashima@192.168.1.21>
2016-05-17 12:54:22 +0900

    20160517 beauti/home

--- www/beauti/home/home_top/home_top_ctl.js
+++ www/beauti/home/home_top/home_top_ctl.js
@@ -4,14 +4,44 @@ angular.module('miral.beauti.home.home_top.controllers', [])
 
 	console.log('Start beautiHomeHomeTopControllers');
 
+	//$scope.search = function(){
+		//console.log($state.$current);
 
-	$scope.search = function(){
-		console.log($state.$current);
+		//var transition = $timeout(function(){
+			//$state.go('top.salon-search',null,'');
+		//});
+	//};
 
-		var transition = $timeout(function(){
-			$state.go('top.salon-search',null,'');
-		});
-	};
+	//　遷移　
+	//美容師：ホーム：検索結果（未️マッチ）
+	$scope.beautiHomeSalonSrhrst=function() {
+		$state.go('beauti-home-salon_srhrst',null,'');
+	}
+	
+	//美容師：ホーム：お知らせ詳細
+	$scope.beautiHomeAnnounceDetail=function() {
+		$state.go('beauti-home-announce_detail',null,'');
+	}
+
+	//美容師：ホーム：お知らせ
+	$scope.beautiHomeAnnounce=function() {
+		$state.go('beauti-home-announce',null,'');
+	}
+	
+	//美容師：ホーム：ミラルのてびき
+	$scope.beautiHomeGuidesMiral=function() {
+		$state.go('beauti-home-guides_miral',null,'');
+	}
+	
+	//美容師：ホーム：オーナーのてびき
+	$scope.beautiHomeGuidesOwner=function() {
+		$state.go('beauti-home-guides_owner',null,'');
+	}
+	
+	//美容師：ホーム：ご意見・お問い合わせ
+	$scope.beautiHomeRequest=function() {
+		$state.go('beauti-home-request',null,'');
+	}
 
 })
 ;
--- www/beauti/home/salon_srhrst/salon_srhrst_ctl.js
+++ www/beauti/home/salon_srhrst/salon_srhrst_ctl.js
@@ -2,7 +2,10 @@ angular.module('miral.beauti.home.salon_srhrst.controllers', [])
 
 .controller('beautiHomeSalonSrhrstControllers', function($scope, $state) {
 
-
+	//美容師：ホーム：検索結果（未️マッチ）
+	$scope.beautiHomeSalonDetail=function() {
+		$state.go('beauti-home-salon-detail',null,'');
+	}
 
 
 })
