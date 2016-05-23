0cbbd00af98bba5112edac2b003b6db1f603ad18
y.aoki <y.aoki@192.168.1.52>
2016-05-23 11:29:36 +0900

    修正

--- www/sample/sample1_fnc.js
+++ www/sample/sample1_fnc.js
@@ -10,7 +10,6 @@ angular.module('miral.sample.sample1_fnc', ['miral.common.miralConst'
 		////////////////////////////////////////////////
 		//沿線取得
 		getTrainList:function(pos_, limit_, success_, fail_){
-			u"""沿線取得"""
 			
 			//pos:開始位置1〜、limit:取得数
 			reqMsg = {pos:pos_, limit:limit_};
