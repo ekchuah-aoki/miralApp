2ac5e36a939571e114c93dbc0ed560f94e03d944
m.nagashima <m.nagashima@192.168.1.21>
2016-05-25 15:53:10 +0900

    20160525 beauti/setting/setting_home

--- www/beauti/setting/setting_home/setting_home_ctl.js
+++ www/beauti/setting/setting_home/setting_home_ctl.js
@@ -1,13 +1,49 @@
 angular.module('miral.beauti.setting.setting_home.controllers', [])
 
-.controller('beautiSettingSettingHomeControllers', function($scope) {
+.controller('beautiSettingSettingHomeControllers', function($scope,$state) {
 	
-	/* ユーザー情報 */
+	// ユーザー情報 
 	$scope.user_name = '本田涼子';
 	$scope.user_img ='beauti.png';
 	
-	/* ポイント */
+	// ポイント 
 	$scope.point = '200';
 
+	// -- 遷移 -- 
+	//美容師：設定：ポイント管理
+	$scope.beautiSettingPointMan=function() {
+		$state.go('beauti-setting-point_man',null,'');
+	}
+
+	//美容師：設定：マイプロフィール
+	$scope.beautiSettingBeautiDetail=function() {
+		$state.go('beauti-setting-beauti_detail',null,'');
+	}
+
+	//美容師：設定：レビュー
+	$scope.beautiSettingReview=function() {
+		$state.go('beauti-setting-review',null,'');
+	}
+
+	//美容師：設定：アカウントの設定
+	$scope.beautiSettingAccount=function() {
+		$state.go('beauti-setting-account',null,'');
+	}
+
+	//美容師：設定：利用規約
+	$scope.beautiSettingTerms=function() {
+		$state.go('beauti-setting-terms',null,'');
+	}
+
+	//美容師：設定：プライバシーポリシー
+	$scope.beautiSettingPrivacyPolicy=function() {
+		$state.go('beauti-setting-privacy_policy',null,'');
+	}
+
+	//美容師：設定：特定商取引
+	$scope.beautiSettingSclt=function() {
+		$state.go('beauti-setting-sclt',null,'');
+	}
+	
 })
 ;
